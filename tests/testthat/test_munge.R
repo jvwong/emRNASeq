3f14694e50e5519d63ec6f67bdadf7563676d3cf
Jeffrey Wong <jvwong@outlook.com>
2017-01-19 15:48:50 -0500

    Added mocked gene_model

--- tests/testthat/test_munge.R
+++ tests/testthat/test_munge.R
@@ -2,9 +2,9 @@ library(emRNASeq)
 
 context("metadata munging")
 
-
 ### merge_data
 data_dir <- file.path(getwd(), "data")
+gene_model <- readRDS(file.path(data_dir, "mock_gene_model.rds"))
 metadata_file <- file.path(data_dir, "tep_phenotypes.txt")
 filelist <- c(file.path(data_dir, "MGH-BrCa-H-74_htsqct.txt"),
   file.path(data_dir, "MGH-BrCa-H-68_htsqct.txt"),
@@ -25,49 +25,52 @@ test_that("merge_data requires correct parameters", {
   expect_error(merge_data(metadata_file, species = 3.2,  source_name, target_name, filelist))
 })
 
-sample_merged_se <- emRNASeq::merge_data(metadata_file, species, source_name, target_name, filelist)
+with_mock(
+  get_gene_model = function( data_df, species, source_name, target_name ){ gene_model },
+  sample_merged_se <- emRNASeq::merge_data(metadata_file, species, source_name, target_name, filelist)
+)
 
-# test_that("merge_data produes a SummarizedExperiment", {
-#   expect_is(sample_merged_se, "SummarizedExperiment")
-# })
-#
-# test_that("merge_data result has data", {
-#   expect_gt(dim(SummarizedExperiment::assays(sample_merged_se)$counts)[1], 30000)
-#   expect_equal(dim(SummarizedExperiment::colData(sample_merged_se)), c(length(filelist),1))
-#   expect_gt(dim(SummarizedExperiment::rowData(sample_merged_se))[1], 30000)
-# })
+test_that("merge_data produes a SummarizedExperiment", {
+  expect_is(sample_merged_se, "SummarizedExperiment")
+})
 
-# ### get_gene_model
-# species <- "human"
-# values <- c("1", "2", "3")
-# sample_df <- data.frame(sample = values)
-# source_name <- "ensembl_gene_id"
-# target_name <- "hgnc_symbol"
-#
-# hgnc_symbols <- c("BRCA1", "TP53", "E2F1")
-# ensembl_ids <- c("ENSG00000012048", "ENSG00000141510", "ENSG00000101412")
-# rownames(sample_df) <- ensembl_ids
-#
-# test_that("get_gene_model stops on bad species", {
-#   expect_error(emRNASeq::get_gene_model(sample_df, "asd",  source_name,  target_name))
-# })
-#
-# test_that("get_gene_model stops on bad namespace", {
-#   expect_error(emRNASeq::get_gene_model(sample_df, species,  source_name,  "hgnc_symbols"))
-#   expect_error(emRNASeq::get_gene_model(sample_df, species,  "sensembl_gene_id",  target_name))
-# })
-#
-# gene_model <- emRNASeq::get_gene_model(sample_df, species, source_name,  target_name)
-#
-# test_that("get_gene_model produes a GRanges", {
-#   expect_is(gene_model, "GRanges")
-# })
-#
-# test_that("get_gene_model sets the correct metadata columns", {
-#   expect_equal(names(GenomicRanges::mcols(gene_model)), c(source_name, target_name))
-#   # print(head(GenomicRanges::mcols(gene_model)))
-# })
-#
-# test_that("get_gene_model sets the correct names", {
-#   expect_equal(length(gene_model), length(values))
-# })
+test_that("merge_data result has data", {
+  expect_gt(dim(SummarizedExperiment::assays(sample_merged_se)$counts)[1], 30000)
+  expect_equal(dim(SummarizedExperiment::colData(sample_merged_se)), c(length(filelist),1))
+  expect_gt(dim(SummarizedExperiment::rowData(sample_merged_se))[1], 30000)
+})
+
+### get_gene_model
+species <- "human"
+values <- c("1", "2", "3")
+sample_df <- data.frame(sample = values)
+source_name <- "ensembl_gene_id"
+target_name <- "hgnc_symbol"
+
+hgnc_symbols <- c("BRCA1", "TP53", "E2F1")
+ensembl_ids <- c("ENSG00000012048", "ENSG00000141510", "ENSG00000101412")
+rownames(sample_df) <- ensembl_ids
+
+test_that("get_gene_model stops on bad species", {
+  expect_error(emRNASeq::get_gene_model(sample_df, "asd",  source_name,  target_name))
+})
+
+test_that("get_gene_model stops on bad namespace", {
+  expect_error(emRNASeq::get_gene_model(sample_df, species,  source_name,  "hgnc_symbols"))
+  expect_error(emRNASeq::get_gene_model(sample_df, species,  "sensembl_gene_id",  target_name))
+})
+
+gene_model <- emRNASeq::get_gene_model(sample_df, species, source_name,  target_name)
+
+test_that("get_gene_model produes a GRanges", {
+  expect_is(gene_model, "GRanges")
+})
+
+test_that("get_gene_model sets the correct metadata columns", {
+  expect_equal(names(GenomicRanges::mcols(gene_model)), c(source_name, target_name))
+})
+
+test_that("get_gene_model sets the correct names", {
+  expect_equal(length(gene_model), length(values))
+})
+
--- tests/testthat/test_rnaseqProcess.R
+++ tests/testthat/test_rnaseqProcess.R
@@ -1,52 +1,52 @@
-# library(emRNASeq)
-#
-# context("RNA-seq processing")
-#
-# data_dir <- file.path(getwd(), "data")
-# sample_merged_se <- readRDS(file = file.path(data_dir, "sample_merged_se.rds"))
-#
-# ### filter_rseq
-# test_that("filter_rseq requires two classes", {
-#   expect_error(filter_rseq(sample_merged_se, "HD"))
-# })
-#
-# sample_filtered_dge <- filter_rseq(sample_merged_se, "HD", "BrCa")
-# test_that("filter_rseq requires two classes", {
-#   expect_is(sample_filtered_dge, "DGEList")
-# })
-#
-# ### normalize_rseq
-# tmm_normalized_dge <- normalize_rseq(sample_filtered_dge)
-# test_that("normalize_rseq returns a DGEList", {
-#   expect_is(tmm_normalized_dge, 'DGEList')
-# })
-#
-# ### de_test_rseq
-# test_that("process_rseq returns a DGEList with correct counts attribute", {
-#   expect_equal(dim(tmm_normalized_dge)[2], dim(sample_merged_se)[2])
-# })
-#
-# test_that("de_test_rseq requires two classes", {
-#   expect_error(de_test_rseq(tmm_normalized_dge, c("HD")))
-# })
-#
-# de_tested_tt <- de_test_rseq(tmm_normalized_dge, "HD", "BrCa")
-# test_that("de_test_rseq returns a TopTags with correct attributes", {
-#   expect_gt(dim(de_tested_tt$table)[1], 1)
-# })
-#
-# ### format_ranks_gsea
-# test_that("format_ranks_gsea returns a data.frame", {
-#   expect_is(emRNASeq::format_ranks_gsea(de_tested_tt), 'data.frame')
-# })
-#
-# ### format_expression_gsea
-# test_that("format_expression_gsea returns a data.frame", {
-#   expect_is(emRNASeq::format_expression_gsea(tmm_normalized_dge), 'data.frame')
-# })
-#
-# ### format_class_gsea
-# test_that("format_class_gsea is valid matrix with correct dimensions", {
-#   expect_is(emRNASeq::format_class_gsea(sample_filtered_dge, de_tested_tt), 'matrix')
-#   expect_equal(dim(emRNASeq::format_class_gsea(sample_filtered_dge, de_tested_tt)), c(3,1))
-# })
+library(emRNASeq)
+
+context("RNA-seq processing")
+
+data_dir <- file.path(getwd(), "data")
+sample_merged_se <- readRDS(file = file.path(data_dir, "sample_merged_se.rds"))
+
+### filter_rseq
+test_that("filter_rseq requires two classes", {
+  expect_error(filter_rseq(sample_merged_se, "HD"))
+})
+
+sample_filtered_dge <- filter_rseq(sample_merged_se, "HD", "BrCa")
+test_that("filter_rseq requires two classes", {
+  expect_is(sample_filtered_dge, "DGEList")
+})
+
+### normalize_rseq
+tmm_normalized_dge <- normalize_rseq(sample_filtered_dge)
+test_that("normalize_rseq returns a DGEList", {
+  expect_is(tmm_normalized_dge, 'DGEList')
+})
+
+### de_test_rseq
+test_that("process_rseq returns a DGEList with correct counts attribute", {
+  expect_equal(dim(tmm_normalized_dge)[2], dim(sample_merged_se)[2])
+})
+
+test_that("de_test_rseq requires two classes", {
+  expect_error(de_test_rseq(tmm_normalized_dge, c("HD")))
+})
+
+de_tested_tt <- de_test_rseq(tmm_normalized_dge, "HD", "BrCa")
+test_that("de_test_rseq returns a TopTags with correct attributes", {
+  expect_gt(dim(de_tested_tt$table)[1], 1)
+})
+
+### format_ranks_gsea
+test_that("format_ranks_gsea returns a data.frame", {
+  expect_is(emRNASeq::format_ranks_gsea(de_tested_tt), 'data.frame')
+})
+
+### format_expression_gsea
+test_that("format_expression_gsea returns a data.frame", {
+  expect_is(emRNASeq::format_expression_gsea(tmm_normalized_dge), 'data.frame')
+})
+
+### format_class_gsea
+test_that("format_class_gsea is valid matrix with correct dimensions", {
+  expect_is(emRNASeq::format_class_gsea(sample_filtered_dge, de_tested_tt), 'matrix')
+  expect_equal(dim(emRNASeq::format_class_gsea(sample_filtered_dge, de_tested_tt)), c(3,1))
+})
